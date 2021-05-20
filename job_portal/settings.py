a1e5660ff02765af1601541c5c81acc0e54bf9d7
jyotheeswar114 <jyotheeswarganne60@gmail.com>
2021-05-20 18:18:44 +0530

    created new app and added database

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'job_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'job_portal',
+        'USER': 'root',
+        'PASSWORD': 'jyo12345',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
